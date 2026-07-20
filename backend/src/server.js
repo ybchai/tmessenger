8b639544636393a0052d09a7f205eeab7d322f5f
ybchai <yanbin.chai@gmail.com>
2026-07-20 16:16:10 +0800

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
