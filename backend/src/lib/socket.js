40da522305264b83198171c830ed8b50360e7227
ybchai <yanbin.chai@gmail.com>
2026-07-27 21:15:58 +0800

    debug

---
+++ backend/src/lib/socket.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+export { app, server, io };
