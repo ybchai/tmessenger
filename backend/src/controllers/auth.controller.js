21159cd8a32aa77d1e7426c0f567594ecef8ede4
ybchai <yanbin.chai@gmail.com>
2026-07-27 21:39:20 +0800

    debug auth routes and middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-export async function checkAuth(req, res, next){
-    if(!req.user){
-        return res.status(401).json({ error: "Unauthorized" });
-    }
+export async function checkAuth(req, res, next) {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-    res.status(200).json(req.user));
+  res.status(200).json(req.user);
 }
