ef6441819bcbd6bd1307c92b627c9a5b28b4c935
ybchai <yanbin.chai@gmail.com>
2026-07-28 20:12:57 +0800

    implement socket-io for real time messaging functionality

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,17 +1,34 @@
 import express from "express";
-import { Server } from "socket.io";
 import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
-
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
+const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5173";
+
+const io = new Server(server, { cors: { origin: [allowedOrigin] } });
+
+function getReceiverSocketId(userId) {
+  return userSocketMap[userId];
+}
+
+// online users map = { userId: socketId }
+const userSocketMap = {};
+
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+
+  if (userId) userSocketMap[userId] = socket.id;
+
+  // io.emit() sends event to everyone - broadcast
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  // socket.on is used to listen for events
+  socket.on("disconnect", () => {
+    if (userId) delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
 });
 
-export { app, server, io };
+export { app, server, io, getReceiverSocketId };
