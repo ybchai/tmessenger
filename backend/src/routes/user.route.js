952d3fe9395cd69b2b947c1e04b882ca5f36a794
ybchai <yanbin.chai@gmail.com>
2026-08-04 23:12:14 +0800

    fixed naming error in user.route

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { searchUsers, updatePreferredLanguage } from "../controllers/user.controller.js";
+import { getUsers, updatePreferredLanguage } from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 
 const router = express.Router();
 
-router.get("/search", protectRoute, searchUsers);
+router.get("/search", protectRoute, getUsers);
 
 router.patch("/preferences", protectRoute, updatePreferredLanguage);
 
